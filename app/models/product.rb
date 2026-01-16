1aaceb70a63989f56a38fb68025d952824a24451
susilosardjono <abthalrad@gmail.com>
2026-01-17 03:49:32 +0700

    progress through some pages

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,17 @@
 class Product < ApplicationRecord
   has_one_attached :image
   after_commit -> { broadcast_refresh_later_to "products" }
+  validates :title, :description, :image, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validate :acceptable_image
+  
+  def acceptable_image
+    return unless image.attached?
+
+    acceptable_types = [ "image/gid", "image/jpeg", "image/png" ]
+    unless acceptable_types.include?(image.content_type)
+      errors.add(:image, "must be GIF, JPG, or PNG image")
+    end
+  end
+  validates :title, uniqueness: true
 end
