4b2c30bbaddf153c111d9cfc25a34c35b913f123
susilosardjono <abthalrad@gmail.com>
2026-01-18 22:23:32 +0700

    TASK D done

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,10 @@
 class StoreController < ApplicationController
+
   def index
     @products = Product.order(:title)
+    session[:counter] = (session[:counter] || 0 ) + 1
+
+    @sessionCount = session[:counter]
   end
 end
+
